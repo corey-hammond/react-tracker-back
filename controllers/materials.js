bfe582661606aa4af954d251a2645de86301a219
Corey Hammond <coreyjhammond@gmail.com>
2020-02-19 21:45:49 -0800

    set up Material controller

---
+++ controllers/materials.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const router = express.Router()
+
+const Material = require("../models/Material")
+
+module.exports = router; 
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const materialsController = require("./controllers/materials")
 
 const app = express();
+
+app.use("/materials", materialsController);
 
 app.listen(3000, () => console.log("Running on port 3000"));
